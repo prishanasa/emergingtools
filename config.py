3d6316d29c3092cf5ed41a07a6240dbc4e29fd54
Prisha Nasa <prishanasa14@gmail.com>
2026-03-20 14:13:36 +0530

    Add centralized config file for model and pipeline parameters

---
+++ config.py
@@ -0,0 +1,8 @@
+# Configuration constants
+TOP_K_CHUNKS = 5
+CHUNK_SIZE = 500
+CHUNK_OVERLAP = 100
+EMBED_MODEL = 'all-MiniLM-L6-v2'
+GROQ_MODEL = 'llama-3.3-70b-versatile'
+MAX_TOKENS = 1500
+TEMPERATURE = 0.2
